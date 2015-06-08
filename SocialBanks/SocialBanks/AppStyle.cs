9c2cc1030c04388fd0e62faf2f481fbaf0642a3a
Fabricio Matos <fabriciovargasmatos@gmail.com>
2015-06-08 03:18:49 -0300

    color changed to socialbanks green

--- SocialBanks/SocialBanks/AppStyle.cs
+++ SocialBanks/SocialBanks/AppStyle.cs
@@ -4,7 +4,10 @@ namespace SocialBanks.Shared
 {
 	public static class AppStyle
 	{
-		public static Xamarin.Forms.Color BackgroundColor = Helpers.Color.Blue.ToFormsColor();
+		private const int SOCIALBANKS_GREEN_RGB = 0x44BEB1;
+
+
+		public static Xamarin.Forms.Color BackgroundColor = Helpers.Color.FromHex(SOCIALBANKS_GREEN_RGB).ToFormsColor();
 		public static Xamarin.Forms.Color TextColor = Xamarin.Forms.Color.White;
 	}
 }
