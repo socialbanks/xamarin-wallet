5ecbf9de40a51d77569d78d9eb013878cab7f4ab
Fabricio Matos <fabriciovargasmatos@gmail.com>
2015-06-08 01:09:13 -0300

    .gitignore

---
+++ SocialBanks/FMBitcoinLib/TransactionHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FMBitcoinLib
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
---
+++ SocialBanks/SocialBanks/Pages/RootPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SocialBanks
+{
+	public class RootPage : ContentPage
+	{
+		public RootPage ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
